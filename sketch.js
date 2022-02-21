a173bb352f75c9eda770600ccc1b1d2737972f0e
Archit Jain <arcyjain2002@gmail.com>
2022-02-21 18:13:53 +0530

    someithin cool 1

--- sketch.js
+++ sketch.js
@@ -5,5 +5,12 @@ function setup() {
 
 function draw() {
   // put drawing code here
-  background(220, 100, 0, 1);
+  // background(120, 0, 0);
+  // ellipse(50, 50, 80, 80);
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }
