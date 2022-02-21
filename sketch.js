a85b0620e94e83084ea46dee35518bd5a8785d60
Archit Jain <arcyjain2002@gmail.com>
2022-02-22 01:32:50 +0530

    sketch board

--- sketch.js
+++ sketch.js
@@ -1,16 +1,30 @@
-function setup() {
-  // put setup code here
-  createCanvas(400, 400);
-}
+// function setup() {
+//   // put setup code here
+//   createCanvas(400, 400);
+// }
 
-function draw() {
-  // put drawing code here
-  // background(120, 0, 0);
-  // ellipse(50, 50, 80, 80);
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
-}
+// function draw() {
+//   // put drawing code here
+//   // background(120, 0, 0);
+//   // ellipse(50, 50, 80, 80);
+//   if (mouseIsPressed) {
+//     fill(0);
+//   } else {
+//     fill(255);
+//   }
+//   ellipse(mouseX, mouseY, 80, 80);
+// }
+
+// function setup() {
+//   createCanvas(400, 400);
+//   background(0);
+// }
+
+// function draw() {
+//   // noStroke();
+//   // fill(255);
+//   // circle(mouseX, mouseY, 10);
+//   point(mouseX, mouseY);
+//   stroke("white");
+//   strokeWeight(10);
+// }
